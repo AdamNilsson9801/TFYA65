7237d5143b11e6778ce8540a27fa99ca3ae0791d
Adam Nilsson <nilssonadam9801@gmail.com>
2024-09-21 13:40:18 +0200

    Controlls updated

--- UnityProject/LjudProject_TFYA65/Assets/Controlls.cs
+++ UnityProject/LjudProject_TFYA65/Assets/Controlls.cs
@@ -12,6 +12,7 @@ public class Controlls : MonoBehaviour
 
     private Vector3 targetPos;
     private bool isMoving = false;
+    private int lanePos = 1;
 
 
     // Start is called before the first frame update
@@ -25,19 +26,25 @@ public class Controlls : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            targetPos = lanePositions.transform.GetChild(0).position;
-            isMoving = true;
+            int currentLane = lanePos;
+            Debug.Log("currentLane: " + currentLane);
+            if (isMovePossible(currentLane, currentLane - 1))
+            {
+                targetPos = lanePositions.transform.GetChild(currentLane - 1).position;
+                isMoving = true;
+                lanePos = currentLane - 1;
+            }
         }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            targetPos = lanePositions.transform.GetChild(1).position;
-            isMoving = true;
 
-        }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            targetPos = lanePositions.transform.GetChild(2).position;
-            isMoving = true;
+            int currentLane = lanePos;
+            if (isMovePossible(currentLane, currentLane + 1))
+            {
+                targetPos = lanePositions.transform.GetChild(currentLane + 1).position;
+                isMoving = true;
+                lanePos = currentLane + 1;
+            }
 
         }
 
@@ -71,5 +78,28 @@ public class Controlls : MonoBehaviour
             Debug.Log("COLLISION!!");
             GameManager.GetComponent<GameController>().GameOver();
         }
+    }
+
+    private bool isMovePossible(int current, int target)
+    {
+        if (current == 0 || current == 2)
+        {
+            if (target == 1)
+            {
+                return true;
+            }
+            else return false;
+
+        }
+        else if (current == 1)
+        {
+            if (target == 0 || target == 2)
+            {
+                return true;
+            }
+            else return false;
+
+        }
+        return false;
     }
 }
